97d6b5955ef285ca8e79690223a53fb4998bf310
anson <ansonlin12345@gmail.com>
2022-11-15 16:55:47 -0800

    blue top thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialAutos/BlueTop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialAutos/BlueTop.java
@@ -28,40 +28,58 @@ import org.firstinspires.ftc.teamcode.officialAutos.RobotHardware;
 
 public class BlueTop extends LinearOpMode {
     RobotHardware robot = new RobotHardware(this);
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
     @Override
     public void runOpMode() throws InterruptedException {
         robot.initHW();
         waitForStart();
 
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Pose2d start = new Pose2d(-36.4, 61.6, 0);
         drive.setPoseEstimate(start);
 
         Trajectory blueTop = drive.trajectoryBuilder(start)
                 .lineToSplineHeading(new Pose2d(-35.3, 8.3, Math.toRadians(-40)))
-                .waitSeconds(0.5)
                 .back(10)
-                .lineToLinearHeading(new Pose2d(-53.8, 12.0, Math.toRadians(180)))
-                .waitSeconds(0.5)
-                .lineToLinearHeading(new Pose2d(-24, 12, Math.toRadians(-90)))
-                .waitSeconds(0.5)
-                .strafeLeft(12)
-                .back(20)
                 .build();
 
-        Trajectory tagA = drive.trajectoryBuilder(blueTop.end())
+        Trajectory tagA = drive.trajectoryBuilder(cycles.end())
                 .strafeRight(36)
                 .back(20)
                 .build();
 
-        Trajectory tagB = drive.trajectoryBuilder(blueTop.end())
+        Trajectory tagB = drive.trajectoryBuilder(cycles.end())
                 .strafeRight(12)
                 .back(20)
                 .build();
 
-        Trajectory tagC = drive.trajectoryBuilder(blueTop.end())
+        Trajectory tagC = drive.trajectoryBuilder(cycles.end())
                 .strafeLeft(12)
                 .back(20)
                 .build();
+        Trajectory wait = drive.trajectoryBuilder(cycles.end())
+                .build();
+
+
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+        drive.followTrajectory(blueTop);
+
+
+
+    }
+
+    Trajectory cycles;
+    public void cycle(int numCycles) {
+        for (int i = 1; i <= numCycles; i++) {
+            cycles = drive.trajectoryBuilder(new Pose2d(-43.2, 15.3, -40))
+                    .lineToLinearHeading(new Pose2d(-53.8, 12.0, Math.toRadians(180)))
+                    .waitSeconds(0.5)
+                    .lineToLinearHeading(new Pose2d(-24, 12, Math.toRadians(-90)))
+                    .waitSeconds(0.5)
+                    .build();
+        }
     }
 }
