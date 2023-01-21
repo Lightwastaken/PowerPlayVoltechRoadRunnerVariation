52f553a007c48a59147e6a0262f95bc9ec52a031
anson <ansonlin12345@gmail.com>
2023-01-20 20:31:14 -0800

    eli tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/teleop.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.officialAutos.RobotHardware;
 
 @TeleOp(name="teleoptoggles", group="Pushbot")
@@ -20,12 +19,11 @@ public class teleop extends LinearOpMode {
         final double WHEEL_DIAMETER_INCHES = 96.0 / 25.4;     // For figuring circumference
         final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                 (WHEEL_DIAMETER_INCHES * 3.1415);
-        boolean active = true;
 
         double left;
         double right;
         double max;
-        double speedControl;
+        double speedControl = 0.90;
 
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
@@ -52,18 +50,25 @@ public class teleop extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1 );
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 0);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
-            robot.LF.setPower(frontLeftPower);
-            robot.LB.setPower(backLeftPower);
-            robot.RF.setPower(frontRightPower);
-            robot.RB.setPower(backRightPower);
+            robot.LF.setPower(frontLeftPower * speedControl);
+            robot.LB.setPower(backLeftPower * speedControl);
+            robot.RF.setPower(frontRightPower * speedControl);
+            robot.RB.setPower(backRightPower * speedControl);
 
-            // non toggle claws
+            if (gamepad1.x) {
+                speedControl = 0.25;
+            }
+
+            if (gamepad1.y) {
+                speedControl = 1.0;
+            }
+
             if (gamepad1.left_bumper) {
                 robot.claw.setPosition(1);
             }
@@ -72,23 +77,22 @@ public class teleop extends LinearOpMode {
                 robot.claw.setPosition(0);
             }
 
-
-            if (gamepad1.right_trigger > 0.1) {
+            if (gamepad1.right_bumper) {
                 robot.lift(0.2);
-            } else if (gamepad1.right_bumper) {
+            } else if (gamepad1.right_trigger > 0.1) {
                 robot.lift(-0.2);
             } else {
                 robot.lift(0);
 
-                telemetry.addData("LF Encoder", robot.LF.getCurrentPosition());
-                telemetry.addData("LB Encoder", robot.LB.getCurrentPosition());
-                telemetry.addData("RF Encoder", robot.RF.getCurrentPosition());
-                telemetry.addData("RB Encoder", robot.RB.getCurrentPosition());
-                telemetry.addData("LF Inches", robot.LF.getCurrentPosition() / COUNTS_PER_INCH);
-                telemetry.addData("LB Inches", robot.LB.getCurrentPosition() / COUNTS_PER_INCH);
-                telemetry.addData("RF Inches", robot.RF.getCurrentPosition() / COUNTS_PER_INCH);
-                telemetry.addData("RB Inches", robot.RB.getCurrentPosition() / COUNTS_PER_INCH);
-                telemetry.update();
+//                telemetry.addData("LF Encoder", robot.LF.getCurrentPosition());
+//                telemetry.addData("LB Encoder", robot.LB.getCurrentPosition());
+//                telemetry.addData("RF Encoder", robot.RF.getCurrentPosition());
+//                telemetry.addData("RB Encoder", robot.RB.getCurrentPosition());
+//                telemetry.addData("LF Inches", robot.LF.getCurrentPosition() / COUNTS_PER_INCH);
+//                telemetry.addData("LB Inches", robot.LB.getCurrentPosition() / COUNTS_PER_INCH);
+//                telemetry.addData("RF Inches", robot.RF.getCurrentPosition() / COUNTS_PER_INCH);
+//                telemetry.addData("RB Inches", robot.RB.getCurrentPosition() / COUNTS_PER_INCH);
+//                telemetry.update();
 //
 //                // Pace this loop so jaw action is reasonable speed.
                 sleep(50);
